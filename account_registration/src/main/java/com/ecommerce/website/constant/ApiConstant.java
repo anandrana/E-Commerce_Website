5f92b3873811fe8ec21f3c63eac6a576f0ae0d48
Anand kumar rana <ranananad1162@gmail.com>
2023-12-28 09:05:05 +0530

    Remove account_registration folder

--- account_registration/src/main/java/com/ecommerce/website/constant/ApiConstant.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.website.constant;
-
-public class ApiConstant {
-
-	public static final String API_VERSIONING="/v1";
-	public static final String REGISTER="/register";
-	public static final String LOGIN = "/login";
-	
-}
--- account_registration/src/main/java/com/ecommerce/website/constant/ResponseConstant.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.website.constant;
-
-public class ResponseConstant {
-
-	public static final String SUCCESS_STATUS = "SUCCESS";
-	public static final String FAILURE_STATUS = "FAILURE";
-}
